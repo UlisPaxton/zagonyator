434e9677ff62f19f105395f015854fd3c82f21ad
Roman Bodyu <UlisPaxton@users.noreply.github.com>
2021-02-10 16:35:22 +0300

    Update server.py
    Удаление отладочного вывода.

--- server/server.py
+++ server/server.py
@@ -7,13 +7,6 @@ from subprocess import check_output
 from cherrypy.lib import static
 
 
-
-#cmd = f"djoin /provision /domain zdomain.ru /machine edial /savefile C:\\Users\Admin\Desktop\\invites/{computername}.invite".split(' ')
-#print(cmd)
-#os.system(cmd)
-#check_output(cmd, shell=True)
-
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
 print(current_dir)
 invites_dir = current_dir + '\\invites'
@@ -21,14 +14,8 @@ invites_dir = current_dir + '\\invites'
 if not os.path.exists(invites_dir):
     os.mkdir(invites_dir)
 
-
 cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 80})
 
-# conf = {'/invites': {
-#             'tools.staticdir.on':True,
-#             'tools.staticdir.dir': invites_dir,
-#                     }
-#         }
 
 class Zagonyator_WebApp:
 
@@ -42,11 +29,7 @@ class Zagonyator_WebApp:
     def invite(self, computername, domain):
         # run djoin
         cmd = f"djoin /provision /domain {domain} /machine {computername} /savefile {invites_dir}\\{computername}.invite /reuse".split(' ')
-        #cmd = "djoin /provision"
-        print(cmd)
-        #os.system(cmd)
         output = check_output(cmd, shell=True)
-        print(output)
         return static.serve_file(invites_dir + '\\'+ computername +'.invite', 'application/x-download', 'attachment',
                                  computername + '.invite')
 
